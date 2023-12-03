6210b912e64f4ed79b87e7cd76f39eaa84aeb4d4
Ziynatilloh <ziynatillomuhammadjonov@gmail.com>
2023-12-03 11:00:36 +0500

    Modal added

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// Swiper
 const swiperScroll = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
@@ -23,4 +24,27 @@ const swiperScroll = new Swiper(".swiper", {
   autoplay: {
     delay: 5000,
   },
+});
+
+// modal
+const cover = document.querySelector(".cover");
+const modal = document.querySelector(".modal");
+const forum = document.querySelector(".modal__form");
+const closeBtn = document.querySelector(".modal__close");
+const openBtn = document.querySelector(".topnav__btn");
+openBtn.addEventListener("click", () => {
+  cover.classList.remove("modal-block");
+  modal.classList.remove("modal-block");
+});
+closeBtn.addEventListener("click", () => {
+  cover.classList.add("modal-block");
+  modal.classList.add("modal-block");
+});
+cover.addEventListener("click", (e) => {
+  cover.classList.add("modal-block");
+  modal.classList.add("modal-block");
+});
+forum.addEventListener("submit", () => {
+  cover.classList.add("modal-block");
+  modal.classList.add("modal-block");
 });
